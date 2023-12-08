e3eb732155bf3ccb2a5d4d49304f109e5e1e8682
wsGit7 <137900710+wsGit7@users.noreply.github.com>
2023-12-08 18:50:03 +0100

    Add files via upload

---
+++ nowy.js
@@ -0,0 +1,45 @@
+document.getElementById('myfile').addEventListener('change', function() {
+
+
+  var GetFile = new FileReader();
+
+   GetFile .onload=function(){
+        
+  var Zosia = GetFile.result.split('.');
+  // alert(Zosia.length);
+  var i = 0;
+  let anna = document.getElementById("msg").value;
+
+  const timer = ms => new Promise(res => setTimeout(res, ms))
+
+  async function load () { // We need to wrap the loop into an async function for this to work
+    for (var i = anna; i < Zosia.length; i++) {
+      document.getElementById('output').innerHTML= Zosia[i];
+      document.getElementById('output2').innerHTML= "("+i+"/"+Zosia.length+" =  "+((i/Zosia.length)*100).toFixed(2)+" % )";
+      // document.getElementById('output').value= Zosia[i];
+      await timer(3000); // then the created Promise can be awaited
+    }
+  }
+  
+  load();
+  
+    
+
+
+
+
+
+
+  
+
+  
+  
+}
+
+    
+    GetFile.readAsText(this.files[0]);
+})
+
+
+
+ 
